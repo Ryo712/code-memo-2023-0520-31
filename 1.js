3dc437fc3c8e0104bb330d87112724bdcc1a6379
大城亮 <hiltoc.katsu@gmail.com>
2023-06-01 22:07:46 +0900

    if文

---
+++ 1.js
@@ -0,0 +1,14 @@
+if (true) {
+    console.log("この行は実行されます");
+}
+//条件式がtrueであるため、ifの中身が実行される
+
+
+const version = "NAHA";
+if (version === "SHIBUYA") {
+    console.log("TOKYO");
+} else if (version === "NAHA") {
+    console.log("OKINAWA");
+} else if (version === "HAKATA") {
+    console.log("FUKUOKA");
+}
